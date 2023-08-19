9e2954ee758940643f098cbfb7376c837af2ea3d
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2023-08-19 16:35:31 -0400

    Paramaterize correct_exposure

--- burstphoto/exposure/exposure.swift
+++ burstphoto/exposure/exposure.swift
@@ -14,48 +14,32 @@ let max_y_state = try! device.makeComputePipelineState(function: mfl.makeFunctio
 /// By lifting the shadows they suffer less from quantization errors, this is especially beneficial as the bit-depth of the image decreases.
 ///
 /// Inspired by https://www-old.cs.utah.edu/docs/techreports/2002/pdf/UUCS-02-001.pdf
-func correct_exposure(_ final_texture: MTLTexture, _ white_level: Int, _ black_level: [[Int]], _ exposure_control: String, _ exposure_bias: [Int], _ uniform_exposure: Bool, _ color_factors: [[Double]], _ ref_idx: Int) {
+func correct_exposure(_ final_texture: MTLTexture, _ white_level: Int, _ black_levels: [[Int]], _ exposure_control: String, _ exposure_bias: [Int], _ uniform_exposure: Bool, _ color_factors: [[Double]], _ mosaic_pattern_width: Int, _ ref_idx: Int) {
               
     // only apply exposure correction if reference image has an exposure, which is lower than the target exposure
-    if (exposure_control != "Off" && white_level != -1 && black_level[0][0] != -1) {
-          
-        var final_texture_blurred = blur(final_texture, with_pattern_width: 2, using_kernel_size: 2)
+    if (exposure_control != "Off" && white_level != -1 && black_levels[0][0] != -1) {
+        var final_texture_blurred = blur(final_texture, with_pattern_width: mosaic_pattern_width, using_kernel_size: 2)
         let max_texture_buffer = texture_max(final_texture_blurred)
         
-        // find index of image with longest exposure to use the most robust black level value
-        var exp_idx = 0
-        for comp_idx in 0..<exposure_bias.count {
-             if (exposure_bias[comp_idx] > exposure_bias[exp_idx]) {
-                exp_idx = comp_idx
-            }
-        }
-        
-        var black_level0 = 0.0
-        var black_level1 = 0.0
-        var black_level2 = 0.0
-        var black_level3 = 0.0
-        
-        // if exposure levels are uniform, calculate mean value of all exposures
-        if uniform_exposure {
-            
+        var black_levels_representative: [Float32] = Array(repeating: 0.0, count: mosaic_pattern_width*mosaic_pattern_width)
+        if uniform_exposure { // if exposure levels are uniform, calculate mean value of all exposures
             for comp_idx in 0..<exposure_bias.count {
-                black_level0 += Double(black_level[comp_idx][0])
-                black_level1 += Double(black_level[comp_idx][1])
-                black_level2 += Double(black_level[comp_idx][2])
-                black_level3 += Double(black_level[comp_idx][3])
+                for i in 0..<black_levels[comp_idx].count {
+                    black_levels_representative[i] += Float32(black_levels[comp_idx][i])
+                }
             }
-            
-            black_level0 /= Double(exposure_bias.count)
-            black_level1 /= Double(exposure_bias.count)
-            black_level2 /= Double(exposure_bias.count)
-            black_level3 /= Double(exposure_bias.count)
-            
-        } else {
-            black_level0 = Double(black_level[exp_idx][0])
-            black_level1 = Double(black_level[exp_idx][1])
-            black_level2 = Double(black_level[exp_idx][2])
-            black_level3 = Double(black_level[exp_idx][3])
+            black_levels_representative = black_levels_representative.map{Float32($0 / Float32(black_levels.count))}
+        } else { // find index of image with longest exposure to use the most robust black level value
+            var exp_idx = 0
+            for comp_idx in 0..<exposure_bias.count {
+                 if (exposure_bias[comp_idx] > exposure_bias[exp_idx]) {
+                    exp_idx = comp_idx
+                }
+            }
+            black_levels_representative = black_levels[exp_idx].map {Float32($0)}
         }
+        let black_levels_buffer = device.makeBuffer(bytes: black_levels_representative,
+                                                    length: MemoryLayout<Float32>.size * black_levels_representative.count)!
         
         let command_buffer = command_queue.makeCommandBuffer()!
         command_buffer.label = "Correct Exposure"
@@ -66,29 +50,37 @@ func correct_exposure(_ final_texture: MTLTexture, _ white_level: Int, _ black_l
         let threads_per_thread_group = get_threads_per_thread_group(state, threads_per_grid)
        
         if (exposure_control=="Curve0EV" || exposure_control=="Curve1EV") {
-            let color_factor_mean = 0.25*(color_factors[ref_idx][0]+2.0*color_factors[ref_idx][1]+color_factors[ref_idx][2])
+            let color_factor_mean: Double
+            if (mosaic_pattern_width == 6) {
+                color_factor_mean = (8.0*color_factors[ref_idx][0] + 20.0*color_factors[ref_idx][1] + 8.0*color_factors[ref_idx][2]) / 36.0
+            } else if (mosaic_pattern_width == 2) {
+                color_factor_mean = (    color_factors[ref_idx][0] +  2.0*color_factors[ref_idx][1] +     color_factors[ref_idx][2]) /  4.0
+            } else {
+                color_factor_mean = (    color_factors[ref_idx][0] +      color_factors[ref_idx][1] +     color_factors[ref_idx][2]) /  3.0
+            }
             final_texture_blurred = blur(final_texture, with_pattern_width: 1, using_kernel_size: 1)
             
             command_encoder.setTexture(final_texture_blurred, index: 0)
-            command_encoder.setTexture(final_texture, index: 1)
-            command_encoder.setBytes([Int32(exposure_bias[ref_idx])], length: MemoryLayout<Int32>.stride, index: 0)
-            command_encoder.setBytes([Int32(exposure_control=="Curve0EV" ? 0 : 100)], length: MemoryLayout<Int32>.stride, index: 1)
-            command_encoder.setBytes([Float32(white_level)], length: MemoryLayout<Float32>.stride, index: 2)
-            command_encoder.setBytes([Float32(black_level0)], length: MemoryLayout<Float32>.stride, index: 3)
-            command_encoder.setBytes([Float32(black_level1)], length: MemoryLayout<Float32>.stride, index: 4)
-            command_encoder.setBytes([Float32(black_level2)], length: MemoryLayout<Float32>.stride, index: 5)
-            command_encoder.setBytes([Float32(black_level3)], length: MemoryLayout<Float32>.stride, index: 6)
-            command_encoder.setBytes([Float32(color_factor_mean)], length: MemoryLayout<Float32>.stride, index: 7)
-            command_encoder.setBuffer(max_texture_buffer, offset: 0, index: 8)
+            command_encoder.setTexture(final_texture,         index: 1)
+            
+            command_encoder.setBytes([Int32(exposure_bias[ref_idx])],                   length: MemoryLayout<Int32>.stride,   index: 0)
+            command_encoder.setBytes([Int32(exposure_control=="Curve0EV" ? 0 : 100)],   length: MemoryLayout<Int32>.stride,   index: 1)
+            command_encoder.setBytes([Int32(mosaic_pattern_width)],                     length: MemoryLayout<Int32>.stride,   index: 2)
+            command_encoder.setBytes([Float32(white_level)],                            length: MemoryLayout<Float32>.stride, index: 3)
+            command_encoder.setBytes([Float32(color_factor_mean)],                      length: MemoryLayout<Float32>.stride, index: 4)
+            
+            command_encoder.setBuffer(black_levels_buffer, offset: 0, index: 5)
+            command_encoder.setBuffer(max_texture_buffer,  offset: 0, index: 6)
         } else {
             command_encoder.setTexture(final_texture, index: 0)
-            command_encoder.setBytes([Float32(white_level)], length: MemoryLayout<Float32>.stride, index: 0)
-            command_encoder.setBytes([Float32(black_level0)], length: MemoryLayout<Float32>.stride, index: 1)
-            command_encoder.setBytes([Float32(black_level1)], length: MemoryLayout<Float32>.stride, index: 2)
-            command_encoder.setBytes([Float32(black_level2)], length: MemoryLayout<Float32>.stride, index: 3)
-            command_encoder.setBytes([Float32(black_level3)], length: MemoryLayout<Float32>.stride, index: 4)
-            command_encoder.setBuffer(max_texture_buffer, offset: 0, index: 5)
-            command_encoder.setBytes([Float32(exposure_control=="LinearFullRange" ? -1.0 : 2.0)], length: MemoryLayout<Float32>.stride, index: 6)
+            
+            command_encoder.setBytes([Int32(mosaic_pattern_width)],                                 length: MemoryLayout<Int32>.stride,   index: 0)
+            command_encoder.setBytes([Float32(white_level)],                                        length: MemoryLayout<Float32>.stride, index: 1)
+            command_encoder.setBytes([Float32(exposure_control=="LinearFullRange" ? -1.0 : 2.0)],   length: MemoryLayout<Float32>.stride, index: 2)
+            
+            command_encoder.setBuffer(black_levels_buffer, offset: 0, index: 3)
+            command_encoder.setBuffer(max_texture_buffer,  offset: 0, index: 4)
+            
         }
         command_encoder.dispatchThreads(threads_per_grid, threadsPerThreadgroup: threads_per_thread_group)
         command_encoder.endEncoding()
